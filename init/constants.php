39b139c2a48982d2c7be0222b6515a38290a621e
ZedZeroth <jhaywood@protonmail.com>
2022-10-05 21:27:04 +0100

    Delete init directory

--- init/constants.php
+++
@@ -1,5 +0,0 @@
-<?php declare(strict_types=1);
-
-define('BASE_DIR', __DIR__ . '/../src/');
-define('BUY_CSV', __DIR__ . '/../../cra_data/buy.csv');
-define('LBC_CSV', __DIR__ . '/../../cra_data/lbc.csv');
